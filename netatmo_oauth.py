29643ffd0b686dbe295f2cbb103b6860f237811d
Peter Sturm <sturm@uni-trier.de>
2022-09-29 15:44:50 +0200

    A first working prototype

--- netatmo_oauth.py
+++ netatmo_oauth.py
@@ -1,20 +1,19 @@
 import json
-import time
+import random
 import argparse
 import requests
 import re
+import os.path
 
 from datetime import datetime
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
-hostName = "127.0.0.1"
-serverPort = 4344
 
-# Relevant URLs to access Netatmo REST API
-netatmo_oauth2_ep = "https://api.netatmo.com/oauth2/authorize"
-netatmo_token_ep = "https://api.netatmo.com/oauth2/token"
-
-auth_data = dict()
+config = {
+    'host_ip' : '127.0.0.1',
+    'host_port' : 4344,
+    'oauth2_ep' : "https://api.netatmo.com/oauth2/authorize",
+    'token_ep' : "https://api.netatmo.com/oauth2/token"
+}
 
 # Reading the content of the given JSON encoded file; returns a dictionary
 def read_json_file(filename):
@@ -33,61 +32,47 @@ def rest_request (f,url,headers,body):
         # TODO maybe there is more in response that should be returned
         return { "success" : False, "content" : response.status_code }
 
-def get_netatmo_access_token(auth_data,code):
+def get_netatmo_access_token(config,code):
     body = {
         'grant_type' : 'authorization_code',
-        'client_id' : auth_data['client_id'],
-        'client_secret' : auth_data['client_secret'],
+        'client_id' : config['client_id'],
+        'client_secret' : config['client_secret'],
         'code' : code,
-        'redirect_uri' : f"http://{hostName}:{serverPort}/redirect",
+        'redirect_uri' : f"http://{config['host_ip']}:{config['host_port']}/redirect",
         'scope' : 'read_station'
         }
     headers = { 'Content-Type':'application/x-www-form-urlencoded' }
-    return rest_request(requests.post,netatmo_token_ep,headers,body)
+    return rest_request(requests.post,config['token_ep'],headers,body)
 
-def redirect_to_netatmo(oauth2_url,app_id):
-    params = dict()
-    params["client_id"] = app_id
-    params["redirect_uri"] = "abc"
-    params["scope"] = "read_station"
-    params["state"] = "xyz"
-    headers = dict()
-    data = dict()  
-    r = requests.post(oauth2_url,params=params,headers=headers,data=data,allow_redirects=True)
-    return r
+def write_access_token(token,config):
+    content = {
+        'client_id' : config['client_id'],
+        'client_secret' : config['client_secret']
+        }
+    for k in token.keys():
+        content[k] = token[k]
+    t = json.dumps(content, indent=2)
+    filename = config['token_path']
+    t_dir = os.path.dirname(os.path.abspath(filename))
+    if not os.path.isdir(t_dir):
+        os.makedirs(t_dir)
+    with open(filename,'w') as fd:
+        fd.write(t)
 
-def create_netatmo_oauth_url(base_url,app_id):
-    url = f'{base_url}?client_id={app_id}&redirect_uri=http://{hostName}:{serverPort}/redirect&scope=read_station&state=xyz'
-    return url
+class OAuth2Handler(BaseHTTPRequestHandler):
+    """Derived class that handles the OAuth2 authentication process"""
 
-def extract_state_and_code(path,expected_state):
-    # Example http://127.0.0.1:4344/redirect?state=xyz&code=23f24d978404c0b165b0350240508786
-    pattern = "^.*state=(.+)&code=(.+)$"
-    r = re.match(pattern,path)
-    if r and r.group(1) == expected_state:
-        return r.group(2)
-    return None
-
-class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        global auth_data
+        global config
         if self.path == '/':
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.write("<html><head><title>CoReef - Netatmo OAuth</title></head>")
-            self.write("<body>")
-            self.write("<p>Redirecting to netatmo:</p>")
-            self.write(f"<a href='/netatmo'>{netatmo_oauth2_ep} method='post'</a>")
-            self.write("</body></html>")
+            self.handle_path_root()
         elif self.path == "/netatmo":
-            url = create_netatmo_oauth_url(netatmo_oauth2_ep,auth_data['client_id'])
-            self.send_response(307)
-            self.send_header("Location", url)
-            self.end_headers()
+            self.handle_path_netatmo()
         elif self.path.startswith("/redirect"):
-            code = extract_state_and_code(self.path,"xyz")
-            token = get_netatmo_access_token(auth_data,code)
+            code = self.extract_state_and_code(self.path,config['state'])
+            token = get_netatmo_access_token(config,code)
+            if token['success']:
+                write_access_token(token['content'],config)
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -96,32 +81,65 @@ class MyServer(BaseHTTPRequestHandler):
             self.write(f"<p>Token is <{token}></p>")
             self.write("</body></html>")            
         else:
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.write("<html><head><title>CoReef - Netatmo OAuth</title></head>")
-            self.write("<body>")
-            self.write(f"<p>Unknown path {self.path}.</p>")
-            self.write("<p>For debug purposes no error code 404 is returned :-)")
-            self.write("</body></html>")
+            self.handle_path_unknown()
             # self.send_error(404)
 
     def write(self,s):
         self.wfile.write(bytes(s,"utf-8"))
 
+    @staticmethod
+    def extract_state_and_code(path,expected_state):
+        # Example http://127.0.0.1:4344/redirect?state=xyz&code=23f24d978404c0b165b0350240508786
+        pattern = "^.*state=(.+)&code=(.+)$"
+        r = re.match(pattern,path)
+        if r and r.group(1) == expected_state:
+            return r.group(2)
+        return None
+
+    def handle_path_root(self):
+        global config
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.write("<html><head><title>CoReef - Netatmo OAuth</title></head>")
+        self.write("<body>")
+        self.write("<p>Redirecting to netatmo:</p>")
+        self.write(f"<a href='/netatmo'>{config['oauth2_ep']} method='post'</a>")
+        self.write("</body></html>")
+
+    def handle_path_netatmo(self):
+        global config
+        config['state'] = f'CoReef{str(random.randint(1,100000))}'
+        url = f"{config['oauth2_ep']}?client_id={config['client_id']}&redirect_uri=http://{config['host_ip']}:{config['host_port']}/redirect&scope=read_station&state={config['state']}"
+        self.send_response(307)
+        self.send_header("Location", url)
+        self.end_headers()
+
+    def handle_path_unknown(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.write("<html><head><title>CoReef - Netatmo OAuth</title></head>")
+        self.write("<body>")
+        self.write(f"<p>Unknown path {self.path}.</p>")
+        self.write("<p>For debug purposes no error code 404 is returned :-)")
+        self.write("</body></html>")
+
 def main():
-    global auth_data
+    global config
     # Defining and parsing all the arguments used by this script
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", type=str, required=True, help="The file containing all the authentication data (JSON encoded)")
-    parser.add_argument("--path", type=str, required=False, help="The location where to store the access token",default=".")
+    parser.add_argument("--path", type=str, required=False, help="The filename to store the access token",default=".")
     args = parser.parse_args()
-    token_file_path = args.path
+    config['token_path'] = args.path
     # Load authentication data as a dictionary
     auth_data = read_json_file(args.file)
+    config['client_id'] = auth_data['client_id']
+    config['client_secret'] = auth_data['client_secret']
 
     print("netatmo_oauth started")
-    webServer = HTTPServer((hostName, serverPort), MyServer)
+    webServer = HTTPServer((config['host_ip'], config['host_port']), OAuth2Handler)
 
     try:
         webServer.serve_forever()
